973116370322993f02278ae1c67c991cfafd835e
amygori <amydgori@gmail.com>
2014-07-16 21:53:12 -0400

    first draft of full structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
-console.log('The Iron Yard Rocks');
+//the model
+var Sign = Backbone.Model.extend({
+	defaults: function() {
+		return {
+		name: "",
+		element: "",
+		dates: "",
+		imgURL: ""
+		
+		}
+	},
+	urlRoot: "scripts/data.js" 
+	
+});
+//instantiate the model
+var sign = new Sign();
+
+//the collection
+var Zodiac = Backbone.Collection.extend({
+	model: Sign,
+	url: "scripts/data.js"
+});
+
+//set up the routes
+(function (){
+	var AppRouter = Backbone.Router.extend({
+		routes: {
+			'': 
+		}
+	});
+})();
